e2fd52dfdb9f52a284cafdb214a2f449d921b32f
Vivek <vivrkprajapati9986@gmail.com>
2023-08-30 14:49:25 +0530

    - Completed Hero Section

---
+++ src/app/Hero.jsx
@@ -0,0 +1,44 @@
+function Hero() {
+    return ( 
+        <>
+
+        <div className="Hero h-[100vh] w-[100%] px-9 pb-9">
+            <div className="h-[90vh] w-[100%] bg-[#EEEBFE] rounded-3xl flex items-center p-12">
+                <div className="w-[50%]" >
+                    <p className="text-base text-gray-900 font-medium mb-5">Ahead App</p>
+                    <p className="text-6xl font-bold mb-5 leading-[1.1]" >
+                        Master your life by mastering emotions.
+                    </p>
+                    <div className="flex gap-10">
+                        <button className="bg-black text-white rounded-xl px-9 py-3 ]">Download App</button>
+                        <p className="text-left">
+                            <span className="block">⭐ ⭐ ⭐ ⭐ ⭐</span>
+                            <span className="text-gray-800 ml-1 block">100+ AppStore reviews</span>   
+                        </p>
+                    </div>
+                </div>
+                <div className="w-[50%] flex items-center justify-center">
+                    <div className="mobile-phone w-[180px] h-[360px] border-black border-4 rounded-2xl bg-[url('/Poster.png')] bg-[#FFEEE1] bg-center bg-cover"></div>
+                </div>
+            </div>  
+        </div> 
+        <div className="flex justify-evenly items-center px-18 pb-6" >
+            <p className="text-2xl font-semibold w-[300px] h-[200px]">
+                EQ Beats IQ
+            </p>
+
+            <p className="text-base text-gray-600 font-medium w-[300px] h-[200px]">
+                People with high emotional intelligence (EQ) live more fullfilled life.
+                They  tend to be happier and have healthier relationships.
+            </p>
+
+            <p className="text-base text-gray-600 font-medium w-[300px] h-[200px]">
+                They are more successful at their persuit and make for inspiring leaders.
+                According to research, they earn $29k a year.
+            </p>
+        </div>
+        </>
+     );
+}
+
+export default Hero;
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,10 @@
 import Nav from "./Nav"
+import Hero from "./Hero"
 export default function Home() {
   return (
+    <>
     <Nav/>
+    <Hero/>
+    </>
   )
 }
