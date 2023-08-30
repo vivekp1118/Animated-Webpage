c2302309636c453cc0b14e1543e6dba0d8d07039
Vivek <vivrkprajapati9986@gmail.com>
2023-08-30 19:35:29 +0530

    - Added cards and some images

---
+++ src/app/HabitsCard.jsx
@@ -0,0 +1,46 @@
+import EmojiCard from "@/Components/EmojiCards";
+function HabitsCard() {
+    return (
+        <div className="flex flex-col" >
+            <div className="mb-8 px-14">
+                <span className="font-semibold text-3xl inline-block">Does this sound fimiliar...</span>
+                <span className="inline-block h-14 w-14 bg-[url('/SadCat.png')] bg-center bg-cover ml-4"> </span>
+            </div>
+
+            <div className="overflow-x-scroll pb-10">
+            <div className="flex flex-row gap-8 flex-nowrap">
+
+                <EmojiCard
+                    color="#6443E9"
+                    isDark={true}
+                    emoji="😧"
+                    header="You argue with a colleague"
+                    text="You get angry and defensive, instead of staying open and working towards common ground."
+                />
+                <EmojiCard
+                    color="#EEEBFE"
+                    emoji="😠"
+                    header="You argue with a colleague"
+                    text="You get angry and defensive, instead of staying open and working towards common ground."
+                />
+                <EmojiCard
+                    color="#FFEFD5"
+                    emoji="🤔"
+                    header="You get a promotion at work"
+                    text="You question yourself and wonder when they'll realize you're an unqualified imposter, instead of trusting yourself & your abilities."
+                />
+                <EmojiCard
+                    color="#d8f2ff"
+                    emoji="😧"
+                    header="You argue with a colleague"
+                    text="You get angry and defensive, instead of staying open and working towards common ground."
+                />
+            </div>
+            </div>
+        </div>
+
+    );
+
+}
+
+export default HabitsCard;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Nav from "./Nav"
 import Hero from "./Hero"
+import HabitsCard from "./HabitsCard"
 export default function Home() {
   return (
     <>
     <Nav/>
     <Hero/>
+    <HabitsCard/>
     </>
   )
 }
