49b143149988c57547283da65c152a0768d0e0a4
Vivek <vivrkprajapati9986@gmail.com>
2023-08-31 14:11:06 +0530

    -Added About Page

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import Info from "./Info"
 import Scroller from "./ImprovementScroller"
 import Assesment from "./Assesmment"
 import Test from "./Test"
+import About from "./About"
 export default function Home() {
   return (
     <>
@@ -15,6 +16,7 @@ export default function Home() {
     <Scroller/>
     <Assesment/>
     <Test/>
+    <About/>
     </>
   )
 }
