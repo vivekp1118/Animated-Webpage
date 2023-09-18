6fe131c612d4f475500a369e9f90efcf15ea1c6c
Vivek <vivrkprajapati9986@gmail.com>
2023-09-18 17:46:54 +0530

    - Updated Navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,18 @@
 function Navbar() {
   return (
-    <nav className="bg-white text-black flex items-center justify-between h-24 px-14">
+    <nav className="flex h-24 items-center justify-between bg-white px-6 text-black md:px-14">
       <div className="h-14 w-14 rounded-xl bg-violet-700 text-center text-3xl leading-[3.5rem]">
         😁
       </div>
-      <div className="flex gap-9 font-normal">
-        <span className="nav-links cursor-pointer">Emotions</span>
-        <span className="nav-links cursor-pointer">Menifesto</span>
-        <span className="nav-links cursor-pointer">Self Awareness Test</span>
-        <span className="nav-links cursor-pointer">Work With Us</span>
-      </div>
-      <div className=" h-12 w-44 text-center text-white font-medium leading-[3rem] bg-black rounded-3xl">
+      {window.isMobile && (
+        <div className="flex gap-9 font-normal">
+          <span className="nav-links cursor-pointer">Emotions</span>
+          <span className="nav-links cursor-pointer">Menifesto</span>
+          <span className="nav-links cursor-pointer">Self Awareness Test</span>
+          <span className="nav-links cursor-pointer">Work With Us</span>
+        </div>
+      )}
+      <div className=" h-12 w-44 rounded-3xl bg-black text-center font-medium leading-[3rem] text-white">
         {" "}
         Download app
       </div>
